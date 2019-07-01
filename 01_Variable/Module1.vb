47c9d747a282a285f2e644199ec1d2ab849f80d5
171067 <171067@trinityschools.nz>
2019-07-02 10:38:56 +1200

    Adding Variable

--- 01_Variable/Module1.vb
+++ 01_Variable/Module1.vb
@@ -5,6 +5,23 @@
 
         Dim Var14b13 As String = "Weird var name"
 
+        Dim strMyName As String = "Harry"
+
+        Const intMyAge As Integer = 15
+
+        Dim dblDouble As Integer = 4.567
+
+        Console.WriteLine(MYMESSAGE)
+
+        Console.WriteLine(Var14b13)
+
+        Console.WriteLine(strMyName)
+
+        Console.WriteLine(intMyAge)
+
+        Console.WriteLine(dblDouble)
+
+        Console.ReadLine()
     End Sub
 
 
