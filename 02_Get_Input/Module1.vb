33f0df6bcd0afa052619e1f2525f95512e90a871
171067 <171067@trinityschools.nz>
2019-07-03 14:58:12 +1200

    FFF

--- 02_Get_Input/Module1.vb
+++ 02_Get_Input/Module1.vb
@@ -7,22 +7,27 @@
     ''' 
     Sub Main()
         ' Housekeeping
-
+        Dim Usersname, outputMessage As String
+        Dim Firstnumber, Secoundnumber, sum, product As Integer
 
         ' Ask for user name
-
+        Console.WriteLine("What is your name")
+        Usersname = Console.ReadLine()
 
         ' Ask user for two numbers
+        Console.WriteLine("Enter a Number")
+        Firstnumber = Console.ReadLine()
 
-
+        Console.WriteLine("Enter another number")
+        Secoundnumber = Console.ReadLine()
         ' Add numbers together
-
+        sum = Firstnumber + Secoundnumber
 
         ' Multiply numbers together
-
+        product = Firstnumber * Secoundnumber
 
         ' Greet user and display maths
-
+        outputMessage = String.Format("Hello, {0}. The sum of your Numbers equals {1}. The product of your numbers equals {2}." Username, sum)
 
     End Sub
 
