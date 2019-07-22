bab448ab31e9ec9e7027c420ad8fa8026409f7f0
171067 <171067@trinityschools.nz>
2019-07-23 10:07:07 +1200

    work

--- 04_TryCatch/Module1.vb
+++ 04_TryCatch/Module1.vb
@@ -1,21 +1,33 @@
 ﻿''' <summary>
 '''     This program asks the user for a number until they enter a number between 1 and 10.
 ''' </summary>
-''' <author> Mr M </author>
+''' <author> Harry  </author>
 ''' <date> 13Jun19 </date>
 Module Module1
 
     Sub Main()
         'Housekeeping
         Dim UserResponse As Integer
+        Const ERROR_MESSAGE As String = "Please enter an valied interger"
+        Dim Vaildinput As Boolean = False
 
-        ' Ask user for a number and store it.
-        Console.WriteLine("Please enter an integer between 1 and 10.")
-        UserResponse = Console.ReadLine()
+        While Not Vaildinput
+            Try
+                ' Ask user for a number and store it.
+                Console.WriteLine("Please enter an integer between 1 and 10.")
+                UserResponse = Console.ReadLine()
+                Vaildinput = True
 
+            Catch ex As Exception
+                Console.WriteLine(ERROR_MESSAGE)
+
+            End Try
+
+        End While
         ' Pause the program to view output.
+        Dim output As String = String.Format("you entered {0}.", UserResponse)
+        Console.WriteLine(output)
         Console.ReadLine()
 
     End Sub
-
 End Module
